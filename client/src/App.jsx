533b2955d5105023b95cef394bde1f11406608b9
Noah Vaughn <noahdvaughn@gmail.com>
2023-06-29 15:11:14 -0400

    finished sidebar and api video call

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import {Box} from "@mui/material"
-import './App.css'
 import Navbar from './components/Navbar'
 import Feed from './components/Feed'
 import VideoDetail from './components/VideoDetail'
@@ -11,7 +10,7 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Box sx={{backgroundColor: '#000'}}>
+      <Box sx={{backgroundColor: 'black'}}>
         <Navbar/>
       </Box>
       <Routes>
