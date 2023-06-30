12644e6debf786bae033af1c5385e2006d2f46bc
Noah Vaughn <noahdvaughn@gmail.com>
2023-06-30 17:55:54 -0400

    added react-player dependency

--- client/src/components/VideoDetail.jsx
+++ client/src/components/VideoDetail.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import {useEffect, useState} from 'react'
+import {Link, useParams} from 'react-router-dom'
+import ReactPlayer from 'react-player'
+import { Typography, Box, Stack } from '@mui/material'
+import { CheckCircle } from '@mui/icons-material'
+import Videos from './Videos'
+import { fetchFromAPI } from '../utils/fetchFromAPI'
 
 const VideoDetail = () => {
   return (
-    <div>VideoDetail</div>
+    <Box minHeight='95vh'>
+      <Stack direction={{xs: 'column', md: 'row'}}>
+
+      </Stack>
+
+    </Box>
   )
 }
 
