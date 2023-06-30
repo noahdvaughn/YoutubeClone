50e5be840817219149d7aa5d104f902e9c5ca416
Noah Vaughn <noahdvaughn@gmail.com>
2023-06-30 17:33:54 -0400

    completed channel page

--- client/src/components/ChannelCard.jsx
+++ client/src/components/ChannelCard.jsx
@@ -4,10 +4,10 @@ import { CheckCircle } from '@mui/icons-material'
 import { Link } from 'react-router-dom'
 import { demoProfilePicture } from '../utils/constants'
 
-const ChannelCard = ({ channelDetails}) => {
+const ChannelCard = ({ channelDetails, marginTop}) => {
   return (
     <Box sx={{boxShadow: 'none', borderRadius: '20px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: {xs: '356px', md: '320px'}, 
-    height: '326px', margin: 'auto'
+    height: '326px', margin: 'auto', marginTop: marginTop
     }}>
       <Link to={`/channel/${channelDetails?.id?.channelId}`}>
         <CardContent sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center', color: '#fff'}}>
--- client/src/components/ChannelDetail.jsx
+++ client/src/components/ChannelDetail.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
+import {useParams} from 'react-router-dom'
+import {Box} from '@mui/material'
+import Videos from './Videos'
+import ChannelCard from './ChannelCard'
+import { fetchFromAPI } from '../utils/fetchFromAPI'
+
+
 
 const ChannelDetail = () => {
+  const {id} = useParams()
+  const [channelDetails, setChannelDetails] = useState(null)
+  const [videos, setVideos] = useState([])
+
+  useEffect(()=> {
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data)=>{setChannelDetails(data.items[0])})
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then((data)=>{setVideos(data.items)})
+  },[id])
+
   return (
-    <div>ChannelDetail</div>
+    <Box minHeight='95vh'>
+      <Box>
+        <div style={{background: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%)', zIndex: '1', height: '300px'}}/>
+        <ChannelCard channelDetails={channelDetails} marginTop={'-95px'}/>
+      </Box>
+      <Box display='flex' p='2'>
+        <Box sx={{mr: {sm: '100px'}}} />
+          <Videos videos={videos}/>
+        </Box>
+    </Box>
   )
 }
 
