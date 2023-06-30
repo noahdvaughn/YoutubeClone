78d51235cbfcd5059c3e102975ffafcc514d86f0
Noah Vaughn <noahdvaughn@gmail.com>
2023-06-30 17:51:34 -0400

    finished search feed

--- client/src/components/Searchbar.jsx
+++ client/src/components/Searchbar.jsx
@@ -4,9 +4,19 @@ import { Paper, IconButton } from '@mui/material'
 import {Search} from '@mui/icons-material'
 
 const Searchbar = () => {
+  const [searchTerm, setSearchTerm] = useState('')
+  const navigate = useNavigate()
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(searchTerm){
+      navigate(`/search/${searchTerm}`)
+      setSearchTerm('')
+    }
+
+  }
   return (
-    <Paper component='form' onSubmit={()=>{}} sx={{borderRadius: 20, border: '1px solid #e3e3e3', pl: 2, boxShadow: 'none', mr:{sm: 5}}}>
-      <input className='search-bar' placeholder='search...' value='' onChange={()=> {}}/>
+    <Paper component='form' onSubmit={handleSubmit} sx={{borderRadius: 20, border: '1px solid #e3e3e3', pl: 2, boxShadow: 'none', mr:{sm: 5}}}>
+      <input className='search-bar' placeholder='search...' value={searchTerm} onChange={(e)=> { setSearchTerm(e.target.value)}}/>
       <IconButton type='submit' sx={{p: '10px', color: 'red'}}>
         <Search />
       </IconButton>
