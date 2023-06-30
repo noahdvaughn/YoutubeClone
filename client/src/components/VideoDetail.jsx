b172cb245e1f94c03a77e6da43401fe5e0d41522
Noah Vaughn <noahdvaughn@gmail.com>
2023-06-30 18:36:53 -0400

    working app

--- client/src/components/VideoDetail.jsx
+++ client/src/components/VideoDetail.jsx
@@ -7,12 +7,50 @@ import Videos from './Videos'
 import { fetchFromAPI } from '../utils/fetchFromAPI'
 
 const VideoDetail = () => {
+  const [videoDetails, setVideoDetails] = useState(null)
+  const [relatedVideos, setRelatedVideos] = useState([])
+
+const {id} = useParams()
+
+useEffect(()=> {
+  fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data)=> {setVideoDetails(data.items[0])})
+  fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`).then((data)=> {setRelatedVideos(data.items)})
+}, [id])
+
+if (!videoDetails?.snippet) return "Loading..."
+
+const {snippet: {title, channelId, channelTitle}, statistics: {viewCount, likeCount}} = videoDetails
+
   return (
     <Box minHeight='95vh'>
       <Stack direction={{xs: 'column', md: 'row'}}>
+        <Box flex='1'>
+          <Box sx={{width: '100%', position: 'sticky', top: '86px'}}>
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className='react-player' controls/>
+            <Typography color='#FFF' variant='h5' fontWeight='bold' p={2}>{title}</Typography>
+            <Stack direction="row" justifyContent='space-between' sx={{color: '#fff'}} py={1} px={2}>
+              <Link to={`/channel/${channelId}`}>
+                <Typography variant={{sm: 'subtitle1', md: 'h6'}} color='#FFF'>
+                  {channelTitle}
+                  <CheckCircle  sx={{fontSize: '12px', color: 'gray', ml: '5px'}}/>
+                </Typography>
+              </Link>
+              <Stack direction='row' gap='20px' alignItems='center' >
+                <Typography variant='body1' sx={{opacity: '0.7'}}>
+                {parseInt(viewCount).toLocaleString()} views
+                </Typography>
+                <Typography variant='body1' sx={{opacity: '0.7'}}>
+                {parseInt(likeCount).toLocaleString()} likes
+                </Typography>
+              </Stack>
 
+            </Stack>
+          </Box>
+        </Box>
+      <Box px={2} py={{md: 1, xs: 5}} justifyContent='center' alignItems='center'>
+        <Videos videos={relatedVideos} direction={'column'}/>
+      </Box>
       </Stack>
-
     </Box>
   )
 }
--- client/src/utils/constants.jsx
+++ client/src/utils/constants.jsx
@@ -11,14 +11,16 @@ import GraphicEqIcon from '@mui/icons-material/GraphicEq'
 import TheaterComedyIcon from '@mui/icons-material/TheaterComedy'
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter'
 import DeveloperModeIcon from '@mui/icons-material/DeveloperMode'
+import { SportsBaseball, SportsFootball, IceSkating } from '@mui/icons-material'
+
 
 export const logo = 'https://i.ibb.co/s9Qys2j/logo.png'
 
 export const categories = [
   { name: 'New', icon: <HomeIcon /> },
-  { name: 'JS Mastery', icon: <CodeIcon /> },
-  { name: 'Coding', icon: <CodeIcon /> },
-  { name: 'ReactJS', icon: <CodeIcon /> },
+  { name: 'Tampa Bay Rays', icon: <SportsBaseball /> },
+  { name: 'TB Buccaneers', icon: <SportsFootball /> },
+  { name: 'TB Lightning', icon: <IceSkating /> },
   { name: 'NextJS', icon: <CodeIcon /> },
   { name: 'Music', icon: <MusicNoteIcon /> },
   { name: 'Education', icon: <SchoolIcon /> },
