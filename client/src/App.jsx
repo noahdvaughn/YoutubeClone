b82912414851c3de2ce252ef05a1204175d18232
Noah Vaughn <noahdvaughn@gmail.com>
2023-07-04 17:13:58 -0400

    finished searchfeed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import VideoDetail from './components/VideoDetail'
 import ChannelDetail from './components/ChannelDetail'
 import Search from './components/Search'
 import { useState, useContext, createContext } from 'react'
+import SearchFeed from './components/SearchFeed'
 export const ColorContext = createContext() 
 
 function App() {
@@ -19,7 +20,7 @@ function App() {
         <Route path='/' element={<Feed/>}/>
         <Route path='/video/:id' element={<VideoDetail/>}/>
         <Route path='/channel/:id' element={<ChannelDetail/>}/>
-        <Route path='/search/:searchTerm' element={<Search/>}/>
+        <Route path='/search/:searchTerm' element={<SearchFeed/>}/>
       </Routes>
     </BrowserRouter>
     
--- client/src/components/SearchFeed.jsx
+++ client/src/components/SearchFeed.jsx
@@ -4,11 +4,13 @@ import Videos from './Videos'
 
 import { fetchFromAPI } from '../utils/fetchFromAPI'
 import { useParams } from 'react-router-dom'
+import { mainColor } from '../utils/constants'
 
 
 const SearchFeed = () => {
+  console.log('here')
   const [videos, setVideos] = useState([])
-  const searchTerm = useParams()
+  const {searchTerm} = useParams()
 
     useEffect(()=> {
     fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data)=>{setVideos(data.items)})
@@ -17,7 +19,7 @@ const SearchFeed = () => {
   return (
     <Box p={2} sx={{overflowY: 'auto', height: '90vh', flex: 2}}>
     <Typography variant='h4' fontWeight='bold' mb={2} sx={{color: 'white'}}>
-    Result for <span style={{color: '#F31503'}}>{searchTerm}</span> 
+    Result for <span style={{color: mainColor}}>{searchTerm}</span> 
     </Typography>
     <Videos videos={videos} />
   </Box>
